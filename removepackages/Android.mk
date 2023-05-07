59b0d264d6e4e39cee372fef8b18475dd7fc3932
Mohammed Rabil <63334479+rabilrbl@users.noreply.github.com>
2023-05-07 06:45:51 +0530

    Revert: remove of Google markup

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail RecorderPrebuilt Chrome Chrome-Stub FilesPrebuilt MarkupGoogle
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail RecorderPrebuilt Chrome Chrome-Stub FilesPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
